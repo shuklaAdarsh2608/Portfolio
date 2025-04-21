6395c379cff04c0068198d42a8ef629ed430dbc6
Adarsh Shukla <150530437+shuklaAdarsh2608@users.noreply.github.com>
2025-04-21 20:53:28 +0530

    Update AboutPic.jsx

--- src/components/aboutme/AboutPic.jsx
+++ src/components/aboutme/AboutPic.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AboutPic = () => {
   return (
     <div className="h-[500px] w-[300px] relative">
-      <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
+      <div className="h-[400px] w-[300px] rounded-[100px] absolute overflow-hidden">
         <img
           src="/public/Images/about.jpg"
           alt="About me"
