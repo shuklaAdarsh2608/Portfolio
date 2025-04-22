a379438991961c240b5d4380e9ac2f36e50b4661
Adarsh Shukla <150530437+shuklaAdarsh2608@users.noreply.github.com>
2025-04-22 21:51:55 +0530

    fix

--- src/components/aboutme/AboutPic.jsx
+++ src/components/aboutme/AboutPic.jsx
@@ -3,11 +3,12 @@ import React from "react";
 const AboutPic = () => {
   return (
     <div className="h-[500px] w-[300px] relative">
-      <div className="h-[400px] w-[300px] rounded-[100px] absolute overflow-hidden">
+      <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
         <img
           src="src\assets\images\about.jpg"
           alt="About me"
           className="h-full w-auto object-cover"
+          onError={() => console.log('Image failed to load')}
         />
       </div>
       <div className="h-[500px] w-[300px] bg-Orange absolute bottom-[-30px] left-[-30px] rounded-bl-[120px] rounded-tr-[120px] rounded-br-[20px] rounded-tl-[20px] -z-10"></div>
