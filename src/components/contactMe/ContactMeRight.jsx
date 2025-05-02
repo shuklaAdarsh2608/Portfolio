1344c48dca196828c7d34d2533d1837c90cf2062
Adarsh Shukla <150530437+shuklaAdarsh2608@users.noreply.github.com>
2025-05-02 07:27:54 +0530

    main

--- src/components/contactMe/ContactMeRight.jsx
+++ src/components/contactMe/ContactMeRight.jsx
@@ -6,7 +6,7 @@ const ContactMeRight = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-12">
       <img
-        src="/public/Img4.jpg"
+        src="src\assets\images\img4.jpg"
         alt="Contact"
         className="max-w-[300px]"
       />
